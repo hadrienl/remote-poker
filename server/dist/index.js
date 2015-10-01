8c136d8d2268efed700afe53db4017ac7b4e31cb
Hadrien Lanneau <hadrien@lanneau.me>
2015-10-01 10:06:33 +0200

    Save connections in rooms

--- server/dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var webserver = require('./web-server')({
-    host: 'localhost',
-    port: 4321
-}),
-    webSocketServer = require('./websocket-server')(webserver);
--- server/src/services/room.js
+++ server/src/services/room.js
@@ -1,10 +1,57 @@
+var _ = require('lodash');
+
 class Room {
+  _rooms = [];
+
   enter (data, connection, request, server) {
-    console.log('entered room', data.id);
+    console.log(`Entered room #${data.id}`);
+    
+    let room = _.findWhere(this._rooms, { id: data.id});
+
+    if (!room) {
+      room = {
+        id: data.id,
+        connections: []
+      };
+      // TODO : make a class
+      this._rooms.push(room);
+    }
+
+    room.connections.push(connection);
+
+    this._sendMessage(room, {
+      type: 'room.enter',
+      data: {
+        room: room.id,
+        user: connection.user
+      }
+    });
   }
 
   leave (data, connection, request, server) {
-    console.log('leaved room', data.id);
+    console.log(`Leaved room #${data.id}`);
+
+    let room = _.findWhere(this._rooms, { id: data.id});
+
+    if (!room) {
+      return;
+    }
+
+    this._room.connections.splice(this._room.indexOf(connection), 1);
+
+    this._sendMessage(room, {
+      type: 'room.leave',
+      data: {
+        room: room.id,
+        user: connection.user
+      }
+    });
+  }
+
+  _sendMessage (room, message) {
+    room.connections.forEach(connection => {
+      connection.sendUTF(JSON.stringify({ type: 'room.enter', data: connection.user }));
+    });
   }
 }
 
